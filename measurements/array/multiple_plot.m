ae1fe11372a687345efb6d5739c04c78a781882c
Lukas Heyn <lukas.heyn@gmail.com>
2025-03-17 22:51:12 +0100

    create multiple plot

---
+++ measurements/array/multiple_plot.m
@@ -0,0 +1,27 @@
+figure
+
+x = 0:0.1:60;
+y = 4.*cos(x)./(x+2);
+
+m = 2;
+n = 10;
+t = tiledlayout(m,n,'TileSpacing','compact');
+
+for i = 1:m
+    bgAx(i) = axes(t,'XTick',[],'YTick',[],'Box','off');
+    bgAx(i).Layout.TileSpan = [1 n];
+    bgAx(i).Layout.Tile = (i - 1) * n + 1;
+
+    for j = 1:n
+        ax(i, j) = axes(t);
+        ax(i, j).Layout.Tile = (i - 1) * n + j;
+        plot(ax(i, j), x, y)
+        ax(i, j).YAxis.Visible = 'off';
+        ax(i, j).Box = 'off';
+        xlim(ax(i, j),[0 15])
+        xlabel(ax(i, j), num2str(j))
+    end
+    ax(i, 1).YAxis.Visible = 'on';
+
+    linkaxes(ax(i, :), 'y')
+end
