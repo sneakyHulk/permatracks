7b6779006c1bcd1696412d640f528a9ca3ec2fd0
Lukas Heyn <lukas.heyn@tum.de>
2025-03-20 13:35:04 +0100

    add position and direction of magnet to test data

--- test/calibrate_and_grab_mag_data_array.cpp
+++ test/calibrate_and_grab_mag_data_array.cpp
@@ -41,6 +41,26 @@ class MagnetometerArrayMagneticFluxDensityDataAverager : public Processor<Messag
 int main() {
 	MagnetometerArrayMagneticFluxDensityDataFilePrinter file_printer;
 
+	{
+		common::print("Without magnet. Press ENTER to continue...");
+		std::cin.ignore();
+		common::println("Wait 5s for the next placement!");
+
+		MagnetometerArraySerialConnection connection("/dev/ttyUSB0", 115200);
+		RawMagnetometerArrayOutput2MagneticFluxDensityData converter;
+		MagnetometerArrayMagneticFluxDensityDataAverager avg;
+
+		connection.asynchronously_connect(converter);
+		converter.asynchronously_connect(avg);
+		avg.synchronously_connect(file_printer);
+
+		auto conn_thread = connection();
+		auto converter_thread = converter();
+		auto avg_thread = avg();
+
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+	}
+
 	auto list = std::initializer_list<std::tuple<int, int, bool>>{{1, 1, true}, {1, 1, false}, {10, 1, true}, {10, 1, false}, {10, 11, true}, {10, 11, false}, {1, 11, true}, {1, 11, false}, {5, 6, true}, {5, 6, false}};
 	auto list2 = std::initializer_list<std::tuple<int, int, bool>>{{1, 1, true}, {1, 1, false}, {11, 1, true}, {11, 1, false}, {11, 10, true}, {11, 10, false}, {1, 10, true}, {1, 10, false}, {5, 6, true}, {5, 6, false}};
 	auto list3 = std::initializer_list<std::tuple<int, int, bool>>{{1, 1, true}, {1, 1, true}, {11, 1, true}, {11, 1, true}, {11, 11, true}, {11, 11, true}, {1, 11, true}, {1, 11, true}, {6, 6, true}, {6, 6, true}};
@@ -65,22 +85,4 @@ int main() {
 
 		std::this_thread::sleep_for(std::chrono::seconds(5));
 	}
-
-	common::print("Last time without magnet. Press ENTER to continue...");
-	std::cin.ignore();
-	common::println("Wait 5s for the next placement!");
-
-	MagnetometerArraySerialConnection connection("/dev/ttyUSB0", 115200);
-	RawMagnetometerArrayOutput2MagneticFluxDensityData converter;
-	MagnetometerArrayMagneticFluxDensityDataAverager avg;
-
-	connection.asynchronously_connect(converter);
-	converter.asynchronously_connect(avg);
-	avg.synchronously_connect(file_printer);
-
-	auto conn_thread = connection();
-	auto converter_thread = converter();
-	auto avg_thread = avg();
-
-	std::this_thread::sleep_for(std::chrono::seconds(5));
 }
