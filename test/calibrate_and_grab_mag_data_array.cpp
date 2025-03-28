0078f41bd66ea77ec894a7ce124853ea64aaf314
Lukas Heyn <lukas.heyn@tum.de>
2025-03-28 17:29:43 +0100

    test data

--- test/calibrate_and_grab_mag_data_array.cpp
+++ test/calibrate_and_grab_mag_data_array.cpp
@@ -15,41 +15,6 @@
 #include "Position.h"
 #include "RawMagnetometerArrayOutput2MagneticFluxDensityData.h"
 
-class MagnetometerArrayMagneticFluxDensityDataAverager2 : public Processor<Message<std::array<MagneticFluxDensityData, 16> >, Message<std::array<MagneticFluxDensityData, 16> > > {
-	Message<std::array<MagneticFluxDensityData, 16> > out{};
-	unsigned int n = 0;
-
-   public:
-	MagnetometerArrayMagneticFluxDensityDataAverager2() = default;
-
-	Message<std::array<MagneticFluxDensityData, 16> > process(Message<std::array<MagneticFluxDensityData, 16> > const &data) override {
-		for (auto i = 0; i < data.size(); ++i) {
-			out[i].x += data[i].x;
-			out[i].y += data[i].y;
-			out[i].z += data[i].z;
-		}
-
-		++n;
-
-		if (stop_token.stop_requested()) {
-			for (auto &[x, y, z] : out) {
-				x /= n;
-				y /= n;
-				z /= n;
-			}
-
-			out.timestamp = data.timestamp;
-			out.src = data.src + "_mean";
-
-			common::println("Avereage consists of ", n, " samples.");
-
-			return out;
-		}
-
-		throw std::logic_error("still waiting");
-	}
-};
-
 class MagnetometerArrayMagneticFluxDensityDataAverager : public Averager<Message<Array<MagneticFluxDensityData, 16> > > {
    public:
 	MagnetometerArrayMagneticFluxDensityDataAverager() : Averager(Message<Array<MagneticFluxDensityData, 16> >{}) {}
@@ -156,14 +121,14 @@ Direction make_direction(char holes_facing, int x_pos, int y_pos, bool in_positi
 }
 
 int main(int argc, char *argv[]) {
-	MagnetometerArrayMagneticFluxDensityDataAverager avg;
+	//MagnetometerArrayMagneticFluxDensityDataAverager avg;
 	UpdaterMagnetometerArrayMagneticFluxDensityDataToMagnetometerArrayMagneticFluxDensityDataWithTruePositionDirection updater(Position{0, 0, 0}, Direction{0, 0});
 	MagnetometerArrayMagneticFluxDensityDataFilePrinterWithTruePositionDirection file_printer;
 
-	avg.asynchronously_connect(updater);
+	//avg.asynchronously_connect(updater);
 	updater.synchronously_connect(file_printer);
 
-	auto avg_thread = avg();
+	//auto avg_thread = avg();
 	auto updater_thread = updater();
 	{
 		common::print("Without magnet. Press ENTER to continue...");
@@ -174,20 +139,24 @@ int main(int argc, char *argv[]) {
 		RawMagnetometerArrayOutput2MagneticFluxDensityData converter;
 
 		connection.asynchronously_connect(converter);
-		converter.synchronously_connect(avg);
+		//converter.synchronously_connect(avg);
+		converter.asynchronously_connect(updater);
 
 		auto conn_thread = connection();
 		auto converter_thread = converter();
 
 		std::this_thread::sleep_for(std::chrono::seconds(3));
 
-		avg.average();
+		//avg.average();
 	}
 
 	auto calibration_pattern = std::initializer_list<std::tuple<char, int, int, bool> >{{'x', 1, 1, true}, {'x', 10, 1, false}, {'y', 11, 1, true}, {'y', 11, 10, false}, {'x', 10, 11, false}, {'x', 1, 11, true}, {'y', 1, 10, false},
 	    {'y', 1, 1, true}, {'y', 6, 5, false}, {'x', 6, 6, true}, {'y', 6, 6, true}, {'x', 5, 6, false}, {'z', 1, 1, true}, {'z', 11, 1, true}, {'z', 11, 11, true}, {'z', 1, 11, true}, {'z', 6, 6, true}};
 
-	for (auto const [holes_facing, x_pos, y_pos, in_positive_direction] : calibration_pattern) {
+	auto pattern = std::initializer_list<std::tuple<char, int, int, bool> >{{'x', 2, 2, true}, {'x', 9, 2, false}, {'y', 10, 2, true}, {'y', 10, 9, false}, {'x', 9, 10, false}, {'x', 2, 10, true}, {'y', 2, 9, false},
+		{'y', 2, 2, true}, {'y', 6, 5, false}, {'x', 6, 6, true}, {'y', 6, 6, true}, {'x', 5, 6, false}, {'z', 2, 2, true}, {'z', 10, 2, true}, {'z', 10, 10, true}, {'z', 2, 10, true}, {'z', 6, 6, true}};
+
+	for (auto const [holes_facing, x_pos, y_pos, in_positive_direction] : pattern) {
 		updater.update(make_position(holes_facing, x_pos, y_pos, in_positive_direction, 11e-2), make_direction(holes_facing, x_pos, y_pos, in_positive_direction));
 
 		common::print("Place the magnet with its vessel with the nibble facing the ", in_positive_direction ? "positive" : "negative", holes_facing, "-direction on (", x_pos, ", ", y_pos, "). Press ENTER to continue...");
@@ -198,13 +167,14 @@ int main(int argc, char *argv[]) {
 		RawMagnetometerArrayOutput2MagneticFluxDensityData converter;
 
 		connection.asynchronously_connect(converter);
-		converter.synchronously_connect(avg);
+		//converter.synchronously_connect(avg);
+		converter.asynchronously_connect(updater);
 
 		auto conn_thread = connection();
 		auto converter_thread = converter();
 
 		std::this_thread::sleep_for(std::chrono::seconds(3));
 
-		avg.average();
+		//avg.average();
 	}
 }
