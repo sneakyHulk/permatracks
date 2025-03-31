545296ba9e407309e2bece1efb5256444a555efd
Lukas Heyn <lukas.heyn@gmail.com>
2025-03-31 07:16:11 +0200

    meeting

--- data_collection/test/test_data_collection.cpp
+++ data_collection/test/test_data_collection.cpp
@@ -16,36 +16,47 @@
 
 template <std::size_t N>
 class CSVSensorArrayReader : public Pusher<Message<Array<MagneticFluxDensityData, N>>> {
-	std::filesystem::path path;
-	csv::CSVReader reader;
-	csv::CSVReader::iterator it;
+	std::filesystem::path _path;
+
+	std::generator<Message<Array<MagneticFluxDensityData, N>>>* gen;
+	decltype(gen->begin()) it;
 
 	std::chrono::time_point<std::chrono::system_clock> _current_time;
 	std::chrono::time_point<std::chrono::system_clock> _sensor_time;
 	std::chrono::time_point<std::chrono::system_clock> next;
 
    public:
-	explicit CSVSensorArrayReader(std::filesystem::path&& path) : path(path), reader(path.string()), it(reader.begin()) {}
+	explicit CSVSensorArrayReader(std::filesystem::path&& path) : _path(std::forward<decltype(path)>(path)), gen(new std::generator<Message<Array<MagneticFluxDensityData, N>>>(get(_path))), it(gen->begin()) {}
+
+	static std::generator<Message<Array<MagneticFluxDensityData, N>>> get(std::filesystem::path path) {
+		csv::CSVReader reader(path.string());
+
+		for (auto const& row : reader) {
+			Message<Array<MagneticFluxDensityData, N>> ret;
+
+			for (auto const& [i, out] : std::ranges::views::enumerate(ret)) {
+				out = {row[common::stringprint('x', i)].template get<double>(), row[common::stringprint('y', i)].template get<double>(), row[common::stringprint('z', i)].template get<double>()};
+			}
+
+			ret.timestamp = row["timestamp"].get<std::uint32_t>();
+			co_yield ret;
+		}
+	}
 
    private:
 	Message<Array<MagneticFluxDensityData, N>> push_once() override {
-		auto first_timestamp = (*it)["timestamp"].get<std::uint32_t>();
+		auto out = *it;
+
 		_current_time = std::chrono::system_clock::now();
-		_sensor_time = next = std::chrono::time_point<std::chrono::system_clock>(std::chrono::milliseconds(first_timestamp));
+		_sensor_time = next = std::chrono::time_point<std::chrono::system_clock>(std::chrono::milliseconds(out.timestamp));
 
 		return push();
 	}
 	Message<Array<MagneticFluxDensityData, N>> push() override {
-		if (++it != reader.end()) {
-			Message<Array<MagneticFluxDensityData, N>> ret;
+		if (++it != gen->end()) {
+			auto ret = *it;
 
-			auto timestamp = (*it)["timestamp"].get<std::uint32_t>();
-
-			for (auto const& [i, out] : std::ranges::views::enumerate(ret)) {
-				out = {(*it)[common::stringprint('x', i)].template get<double>(), (*it)[common::stringprint('y', i)].template get<double>(), (*it)[common::stringprint('z', i)].template get<double>()};
-			}
-
-			if (auto t_new = std::chrono::time_point<std::chrono::system_clock>(std::chrono::milliseconds(timestamp)); t_new < next) {
+			if (auto t_new = std::chrono::time_point<std::chrono::system_clock>(std::chrono::milliseconds(ret.timestamp)); t_new < next) {
 				_current_time = std::chrono::system_clock::now() + std::chrono::seconds(1);
 				_sensor_time = next = t_new;
 
@@ -55,20 +66,25 @@ class CSVSensorArrayReader : public Pusher<Message<Array<MagneticFluxDensityData
 
 			std::this_thread::sleep_until(_current_time + (next - _sensor_time));
 
-			ret.timestamp = timestamp;
-
 			return ret;
 		} else {
-			std::this_thread::sleep_for(std::chrono::seconds(3));
-			reader = csv::CSVReader(path.string());
-			it = reader.begin();
+			delete gen;
+
+			gen = new std::generator<Message<Array<MagneticFluxDensityData, N>>>(get(_path));
+			it = gen->begin();
 
 			return push_once();
 		}
 	}
+
+   public:
+	~CSVSensorArrayReader() { delete gen; }
 };
 
 int main() {
+
+
+
 	CSVSensorArrayReader<16> reader(std::filesystem::path(CMAKE_SOURCE_DIR) / "result" / "LIS3MDL_ARRAY_Besprechungsraum" / "mag_data_LIS3MDL_ARRAY_2025Mar28_17h22min51s_ground_truth_data.txt");
 
 	MagnetometerArrayMagneticFluxDensityDataPrinter printer;
