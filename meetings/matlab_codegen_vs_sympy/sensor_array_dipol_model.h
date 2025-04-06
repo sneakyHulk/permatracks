8e1f6e1899b83390fc24c7466a6816a1ed0ab3a5
Lukas Heyn <lukas.heyn@gmail.com>
2025-04-06 21:50:13 +0200

    matlab sympy test

---
+++ meetings/matlab_codegen_vs_sympy/sensor_array_dipol_model.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+ *                      Code generated with SymPy 1.13.3                      *
+ *                                                                            *
+ *              See http://www.sympy.org/ for more information.               *
+ *                                                                            *
+ *                This file is part of 'SensorArrayDipolModel'                *
+ ******************************************************************************/
+
+
+#ifndef SENSORARRAYDIPOLMODEL__SENSOR_ARRAY_DIPOL_MODEL__H
+#define SENSORARRAYDIPOLMODEL__SENSOR_ARRAY_DIPOL_MODEL__H
+
+void dipol_model(double *B, double *BB, double m1, double x1, double y1, double z1, double theta1, double phi1, double Gx, double Gy, double Gz);
+void dipol_model_jacobian(double *B, double *BB, double m1, double x1, double y1, double z1, double theta1, double phi1, double Gx, double Gy, double Gz);
+
+#endif
+
